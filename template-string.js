a56a3121217738c198305889be15a4d25ec31fe7
Gabriel Succi <zicanony@gmail.com>
2021-07-12 08:54:33 -0300

    Declarando variáveis

---
+++ template-string.js
@@ -0,0 +1,15 @@
+/**
+ * RESULTADO QUE QUEREMOS:
+
+Carlos Gabriel Succi tem 30 anos, pesa 84kg
+tem 1.8 de altura e seu IMC é de 25.92
+Luiz Otávio nasceu em: 
+ */
+
+const nome = "Carlos Gabriel";
+const sobrenome = "Succi";
+const idade = 20;
+const peso = 70;
+const altura = 1.8;
+let imc = peso / (altura * altura) // Usaremos let pois iremos mudar os valores constantemente da variavel
+let anoNascimento = 2021 - idade;
