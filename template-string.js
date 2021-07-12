d03106f05ffa4613cb85ff1707304730a85d88c4
Gabriel Succi <zicanony@gmail.com>
2021-07-12 09:09:31 -0300

    Usando o .toFixed() para diminuir as casas decimais

--- template-string.js
+++ template-string.js
@@ -16,4 +16,10 @@ let anoNascimento = 2021 - idade;
 
 // Template string
 // Basta usar crases para envolver o seu texto e as variaveis com ${variavel}
-console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg \ntem ${altura} de altura e seu IMC é de ${imc}\n${nome} nasceu em: ${anoNascimento}`)
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg \ntem ${altura} de altura e seu IMC é de ${imc.toFixed(2)}\n${nome} nasceu em: ${anoNascimento}`);
+// Para diminuir o numero grande do resultado do IMC usamos o .toFixed().
+// Para usar, baste pegar sua variavel e por o .toFixed(quantasCasasDecimaisVoceDeseja) no fim dela.
+// No caso usei apenas 2 casas decimais
+
+// Façam o teste e aproveitem :D
